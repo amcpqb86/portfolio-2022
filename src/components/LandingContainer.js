7830bdf7eeb41bd249607bf356319021dd608597
Amélien <amcpqb86@gmail.com>
2022-06-02 17:13:49 +0200

    initial commit

---
+++ src/components/LandingContainer.js
@@ -0,0 +1,14 @@
+import "../styles/app.css";
+import React from "react";
+
+function LandingContainer() {
+  return (
+    <div className="landing-container">
+      <h1>
+        Amélien <br /> BERNARD
+      </h1>
+    </div>
+  );
+}
+
+export default LandingContainer;
---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/app.css";
+
+function Menu() {
+  return (
+    <div className="menu">
+      <a href="/">
+        Amelien <br />
+        BERNARD
+      </a>
+      <a href="/contact">Contact</a>
+    </div>
+  );
+}
+
+export default Menu;
